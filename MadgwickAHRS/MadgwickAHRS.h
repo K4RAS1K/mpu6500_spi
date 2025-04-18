d9e1f575e3a13fcbc2801e6b36e2bf86a3413190
Your Name <you@example.com>
2025-04-18 22:57:07 +0300

    fix

--- MadgwickAHRS/MadgwickAHRS.h
+++ MadgwickAHRS/MadgwickAHRS.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //=====================================================================================================
 // MadgwickAHRS.h
 //=====================================================================================================
@@ -35,40 +34,3 @@ void computeAnglesMadgwick();
 //=====================================================================================================
 // End of file
 //=====================================================================================================
-=======
-//=====================================================================================================
-// MadgwickAHRS.h
-//=====================================================================================================
-//
-// Implementation of Madgwick's IMU and AHRS algorithms.
-// See: http://www.x-io.co.uk/node/8#open_source_ahrs_and_imu_algorithms
-//
-// Date			Author          Notes
-// 29/09/2011	SOH Madgwick    Initial release
-// 02/10/2011	SOH Madgwick	Optimised for reduced CPU load
-//
-//=====================================================================================================
-#ifndef MadgwickAHRS_h
-#define MadgwickAHRS_h
-
-#define INSTABILITY_FIX 0
-//----------------------------------------------------------------------------------------------------
-// Variable declaration
-
-#define PI 3.141592
-#define R2D 180.00f/3.141592f
-
-extern volatile float beta;				// algorithm gain
-extern volatile float q0, q1, q2, q3;	// quaternion of sensor frame relative to auxiliary frame
-extern float roll, pitch, yaw;
-//---------------------------------------------------------------------------------------------------
-// Function declarations
-
-void MadgwickAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz);
-void MadgwickAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az);
-void computeAngles();
-#endif
-//=====================================================================================================
-// End of file
-//=====================================================================================================
->>>>>>> d3f1c12d75070e5db16228437c96b7d27941ea7c
